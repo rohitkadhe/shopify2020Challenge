5a754d4f0b97adb8fe946bae01b3b49dfbfe9129
rohitkadhe <rkadhe99@gmail.com>
2021-01-06 15:32:28 -0700

    removed commented code

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
 let knex = require('knex');
-// var conn = {
-//   host: '127.0.0.1',
-//   user: 'postgres',
-//   password: 'root',
-//   charset: 'utf8',
-//   database: 'shopifyimagerepository',
-// };
 knex = knex({
   client: 'pg',
   connection: process.env.DATABASE_URL,
