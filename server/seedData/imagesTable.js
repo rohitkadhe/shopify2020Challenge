1f65c0823b85179501854195c6916aa8672f7446
rohitkadhe <rkadhe99@gmail.com>
2021-01-07 09:58:03 -0700

    Updated seed data

--- server/seedData/imagesTable.js
+++ server/seedData/imagesTable.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    name: 'Image 1',
+    name: 'Seed Image 1',
     public_id: 'ShopifyImageRepo/mj8dahuqmak3keve80bk',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999367/ShopifyImageRepo/mj8dahuqmak3keve80bk.jpg',
@@ -10,7 +10,7 @@ module.exports = [
   },
 
   {
-    name: 'Image 2',
+    name: 'Seed Image 2',
     public_id: 'ShopifyImageRepo/lojcrkibjg9mrq7smpmr',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999377/ShopifyImageRepo/lojcrkibjg9mrq7smpmr.jpg',
@@ -20,7 +20,7 @@ module.exports = [
   },
 
   {
-    name: 'Image 3',
+    name: 'Seed Image 3',
     public_id: 'ShopifyImageRepo/ejrave1morhuenlwases',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999367/ShopifyImageRepo/ejrave1morhuenlwases.jpg',
@@ -30,7 +30,7 @@ module.exports = [
   },
 
   {
-    name: 'Image 4',
+    name: 'Seed Image 4',
     public_id: 'ShopifyImageRepo/jk0wyt0nlzannclvvqrc',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999380/ShopifyImageRepo/jk0wyt0nlzannclvvqrc.jpg',
@@ -40,7 +40,7 @@ module.exports = [
   },
 
   {
-    name: 'Image 5',
+    name: 'Seed Image 5',
     public_id: 'ShopifyImageRepo/zi9hpoisulgupzrynthe',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999377/ShopifyImageRepo/zi9hpoisulgupzrynthe.jpg',
@@ -49,17 +49,17 @@ module.exports = [
     visibility: 'public',
   },
   {
-    name: 'Image 6',
-    public_id: 'ShopifyImageRepo/rmi0gsmnfcjaggidp8fy',
+    name: 'Seed Image 6',
+    public_id: 'ShopifyImageRepo/dt1aeuzutaxojkeka4wa',
     secure_url:
-      'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999655/ShopifyImageRepo/rmi0gsmnfcjaggidp8fy.jpg',
-    uploaded_on: '2021-01-07T02:43:45.290Z',
+      'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999729/ShopifyImageRepo/dt1aeuzutaxojkeka4wa.jpg',
+    uploaded_on: '2021-01-07T02:43:45.293Z',
     user_id: '1',
     visibility: 'public',
   },
   ,
   {
-    name: 'Image 7',
+    name: 'Seed Image 7',
     public_id: 'ShopifyImageRepo/kevfcetxazlch7krpiom',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999647/ShopifyImageRepo/kevfcetxazlch7krpiom.jpg',
@@ -69,22 +69,22 @@ module.exports = [
   },
 
   {
-    name: 'Image 8',
-    public_id: 'ShopifyImageRepo/dt1aeuzutaxojkeka4wa',
-    secure_url:
-      'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999729/ShopifyImageRepo/dt1aeuzutaxojkeka4wa.jpg',
-    uploaded_on: '2021-01-07T02:43:45.293Z',
-    user_id: '1',
-    visibility: 'public',
-  },
-
-  {
-    name: 'Image 9',
+    name: 'Seed Image 8',
     public_id: 'ShopifyImageRepo/gehjt2x3fft2xxrtaftl',
     secure_url:
       'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999724/ShopifyImageRepo/gehjt2x3fft2xxrtaftl.jpg',
     uploaded_on: '2021-01-07T02:43:45.294Z',
     user_id: '1',
     visibility: 'private',
+  },
+
+  {
+    name: 'Seed Image 9',
+    public_id: 'ShopifyImageRepo/rmi0gsmnfcjaggidp8fy',
+    secure_url:
+      'https://res.cloudinary.com/shopifyimagerepository/image/upload/v1609999655/ShopifyImageRepo/rmi0gsmnfcjaggidp8fy.jpg',
+    uploaded_on: '2021-01-07T02:43:45.290Z',
+    user_id: '1',
+    visibility: 'public',
   },
 ];
